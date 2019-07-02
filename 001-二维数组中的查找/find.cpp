e8cfbcb7adebb11a972c617acd647c9e02f81262
bhsimon0810 <52440397+bhsimon0810@users.noreply.github.com>
2019-07-02 14:06:48 +0800

    Create find.cpp

---
+++ 001-二维数组中的查找/find.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool Find(int target, vector<vector<int> > array) {
+        int row = 0;
+        int col = array[0].size() - 1;
+        while(row <= array.size() - 1 && col >= 0) {
+            if(target == array[row][col])
+                return true;
+            else if(target < array[row][col])
+                col--;
+            else if(target > array[row][col])
+                row++;
+        }
+        return false;
+    }
+};

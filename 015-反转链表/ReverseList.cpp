90b30622d6a6800db2b379c3327df0eb6df6d62e
bhsimon0810 <52440397+bhsimon0810@users.noreply.github.com>
2019-07-14 12:27:33 +0800

    Create ReverseList.cpp

---
+++ 015-反转链表/ReverseList.cpp
@@ -0,0 +1,19 @@
+/*
+struct ListNode {
+	int val;
+	struct ListNode *next;
+	ListNode(int x) :
+			val(x), next(NULL) {
+	}
+};*/
+class Solution {
+public:
+    ListNode* ReverseList(ListNode* pHead) {
+        if(pHead == NULL || pHead->next == NULL)
+            return pHead;
+        ListNode *newHead = ReverseList(pHead->next);
+        pHead->next->next = pHead;
+        pHead->next = NULL;
+        return newHead;
+    }
+};

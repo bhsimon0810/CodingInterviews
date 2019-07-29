7395f51d53a2bd2ddbafdabd21a42b88698eedd3
bhsimon0810 <52440397+bhsimon0810@users.noreply.github.com>
2019-07-29 15:41:16 +0800

    Create GetUglyNumber.cpp

---
+++ 033-丑数/GetUglyNumber.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int GetUglyNumber_Solution(int index) {
+        if(index < 7) return index;
+        vector<int> result(index);
+        result[0] = 1;
+        int p2 = 0, p3 = 0, p5 = 0;
+        for(int i = 1; i < index; i++) {
+            result[i] = min(result[p2] * 2, min(result[p3] * 3, result[p5] * 5));
+            if(result[i] == result[p2] * 2) p2++;
+            if(result[i] == result[p3] * 3) p3++;
+            if(result[i] == result[p5] * 5) p5++;
+        }
+        return result[index - 1];
+    }
+};
